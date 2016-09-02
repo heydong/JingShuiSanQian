661fe692925e878d9ac9e31dca0cf11f49ed2c95
heydong <xiaochen1995226@163.com>
2016-09-02 14:21:38 +0800

    some of item detail(drag to continue,popwindow,layout..)

--- app/src/main/java/cn/edu/xmu/jingshuisanqian/ui/activity/MarketDetailActivity.java
+++ app/src/main/java/cn/edu/xmu/jingshuisanqian/ui/activity/MarketDetailActivity.java
@@ -1,21 +1,44 @@
 package cn.edu.xmu.jingshuisanqian.ui.activity;
 
+import android.content.Context;
+import android.graphics.Paint;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Gravity;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import com.loopj.android.image.SmartImageView;
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import butterknife.OnClick;
 import cn.edu.xmu.jingshuisanqian.R;
+import cn.edu.xmu.jingshuisanqian.widget.ImageCycleView;
+import cn.edu.xmu.jingshuisanqian.widget.ItemPopWindow;
 
 public class MarketDetailActivity extends AppCompatActivity {
 
     @Bind(R.id.toolbar)
     Toolbar toolbar;
+    @Bind(R.id.itemPhotos)
+    ImageCycleView imageCycleView;
+    @Bind(R.id.price_now)
+    TextView price_now;
+    @Bind(R.id.price_before)
+    TextView price_before;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,6 +47,9 @@ public class MarketDetailActivity extends AppCompatActivity {
         ButterKnife.bind(this);
         setSupportActionBar(toolbar);
         setTitle("商品详情");
+        initPhotos();
+        price_before.getPaint().setFlags(Paint.STRIKE_THRU_TEXT_FLAG);
+//        toolbar.getBackground().setAlpha(0);//toolbar透明度初始化为0
         toolbar.setNavigationOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -52,4 +78,50 @@ public class MarketDetailActivity extends AppCompatActivity {
         }
         return super.onOptionsItemSelected(item);
     }
+
+    public void initPhotos() {
+        WindowManager wm = (WindowManager) getSystemService(Context.WINDOW_SERVICE);
+        int screenHeight = wm.getDefaultDisplay().getHeight();
+        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, screenHeight / 4);
+        imageCycleView.setLayoutParams(layoutParams);
+        List<ImageCycleView.ImageInfo> imageUrls = new ArrayList<>();
+        imageUrls.add(new ImageCycleView.ImageInfo(R.drawable.tea_image, "ad", ""));
+        imageUrls.add(new ImageCycleView.ImageInfo(R.drawable.tea_image, "ad", ""));
+        imageUrls.add(new ImageCycleView.ImageInfo(R.drawable.tea_image, "ad", ""));
+        imageCycleView.setCycleDelayed(8000);
+        imageCycleView.setTextAvailable(false);
+        imageCycleView.loadData(imageUrls, new ImageCycleView.LoadImageCallBack() {
+            @Override
+            public ImageView loadAndDisplay(ImageCycleView.ImageInfo imageInfo) {
+                SmartImageView smartImageView = new SmartImageView(MarketDetailActivity.this);
+                Picasso.with(MarketDetailActivity.this).load(Integer.parseInt(imageInfo.image.toString())).into(smartImageView);
+                return smartImageView;
+            }
+        });
+    }
+
+    @OnClick(R.id.chooseStandard)
+    public void showPopFormBottom() {
+        ItemPopWindow itemPopWindow = new ItemPopWindow(this, standardClickListener);
+        //showAtLocation(View parent, int gravity, int x, int y)
+        itemPopWindow.showAtLocation(findViewById(R.id.item_content), Gravity.CENTER, 0, 0);
+    }
+
+    @OnClick(R.id.cart)
+    public void cart() {
+        ItemPopWindow itemPopWindow = new ItemPopWindow(this, standardClickListener);
+        //showAtLocation(View parent, int gravity, int x, int y)
+        itemPopWindow.showAtLocation(findViewById(R.id.item_content), Gravity.CENTER, 0, 0);
+    }
+
+    private View.OnClickListener standardClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+
+            switch (v.getId()) {
+
+            }
+        }
+    };
+
 }
---
+++ app/src/main/java/cn/edu/xmu/jingshuisanqian/widget/ItemPopWindow.java
@@ -0,0 +1,73 @@
+package cn.edu.xmu.jingshuisanqian.widget;
+
+/**
+ * Created by hd_chen on 2016/9/2.
+ */
+
+import android.content.Context;
+import android.graphics.drawable.ColorDrawable;
+import android.view.LayoutInflater;
+import android.view.MotionEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.PopupWindow;
+import android.widget.RelativeLayout;
+
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+import cn.edu.xmu.jingshuisanqian.R;
+
+public class ItemPopWindow extends PopupWindow {
+
+    private Context mContext;
+
+    private View view;
+
+    public ItemPopWindow(Context mContext, View.OnClickListener itemsOnClick) {
+
+        this.view = LayoutInflater.from(mContext).inflate(R.layout.item_standard_pop, null);
+        ButterKnife.bind(this,this.view);
+        // 设置外部可点击
+        this.setOutsideTouchable(true);
+        // mMenuView添加OnTouchListener监听判断获取触屏位置如果在选择框外面则销毁弹出框
+        this.view.setOnTouchListener(new View.OnTouchListener() {
+
+            public boolean onTouch(View v, MotionEvent event) {
+
+                int height = view.findViewById(R.id.pop_layout).getTop();
+
+                int y = (int) event.getY();
+                if (event.getAction() == MotionEvent.ACTION_UP) {
+                    if (y < height) {
+                        dismiss();
+                    }
+                }
+                return true;
+            }
+        });
+
+
+    /* 设置弹出窗口特征 */
+        // 设置视图
+        this.setContentView(this.view);
+        // 设置弹出窗体的宽和高
+        this.setHeight(RelativeLayout.LayoutParams.MATCH_PARENT);
+        this.setWidth(RelativeLayout.LayoutParams.MATCH_PARENT);
+
+        // 设置弹出窗体可点击
+        this.setFocusable(true);
+
+        // 实例化一个ColorDrawable颜色为半透明
+        ColorDrawable dw = new ColorDrawable(0xb0000000);
+        // 设置弹出窗体的背景
+        this.setBackgroundDrawable(dw);
+
+        // 设置弹出窗体显示时的动画，从底部向上弹出
+        this.setAnimationStyle(R.style.take_photo_anim);
+    }
+
+    @OnClick(R.id.dismiss)
+    public void dismissPopWindow(){
+        dismiss();
+    }
+}
